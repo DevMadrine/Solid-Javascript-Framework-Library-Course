5b004d1bbf5f41e13a408e9ffc92918cb16f3a8d
DevMadrine <madrinenamulindwa90@gmail.com>
2026-03-02 10:50:57 +0300

    Change ontent in  about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,10 +1,12 @@
 import { Title } from "@solidjs/meta";
+import ClassListStyling from "~/components/ClassListStyling";
+import { ThemedButton } from "~/components/ThemedButton";
 
 export default function About() {
   return (
     <main>
-      <Title>About</Title>
-      <h1>About</h1>
+      <ThemedButton/>
+      <ClassListStyling/>
     </main>
   );
 }
