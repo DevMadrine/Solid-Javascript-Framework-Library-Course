ba91c0f5969da1ca3a652ea747c7466e554013f2
DevMadrine <madrinenamulindwa90@gmail.com>
2026-03-02 11:17:37 +0300

    Added a comment on the implementation of Delegated event handling

--- src/components/EventHandling/DelegatedEventHandling.tsx
+++ src/components/EventHandling/DelegatedEventHandling.tsx
@@ -1,7 +1,7 @@
 export default function DelegatedEventHandling() {
   return(
     <div>
-      //delegated events, not case sensitive
+    {/* delegated events, not case sensitive */}
       <button onClick={() => alert("Button clicked!")}>Click Me</button>
       <button onclick={() => alert("Event object")}>Log Event</button>
     </div>
